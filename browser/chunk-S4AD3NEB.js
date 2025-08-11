e5fae99075d3e44e5ab5fa8c5910a90729835f91
AlexStamatis <st.alexei@hotmail.com>
2025-08-11 12:20:10 +0300

    Auto-generated commit

--- browser/chunk-S4AD3NEB.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-RQ4VVQKS.js";import"./chunk-UNCITDDM.js";import"./chunk-BJMUVMDY.js";import"./chunk-2CIC56OY.js";export{a as SearchResultsComponent};
---
+++ browser/chunk-SU7H2ATG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-S7YVBSYX.js";import"./chunk-PCJYLEWP.js";import"./chunk-GV5T73XR.js";import"./chunk-BUJXTFFG.js";export{a as SearchResultsComponent};
